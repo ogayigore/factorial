be810303bed4dc52e5e1954d2301d39f071bb645
Ирина Цай <ogayigore@macbook-pro-ogaj.local>
2019-02-08 13:34:51 +0300

    Initial Commit

---
+++ factorial viper protocols/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  factorial viper protocols
+//
+//  Created by Игорь Огай on 08/02/2019.
+//  Copyright © 2019 Игорь Огай. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
